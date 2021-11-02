152218f17e8cb021290431cef4dbb07a186b13f5
Vy Hùng <vyvanhung@Vys-MacBook-Pro.local>
2021-11-03 00:05:03 +0700

    Sample2

--- Sample/blog/main.py
+++ Sample/blog/main.py
@@ -3,8 +3,8 @@ from . import schemas,models,database
 from  .schemas import Blog
 from .database import engine, SessionLocal
 from sqlalchemy.orm import Session
-
-
+from typing import List
+from passlib.context import CryptContext
 app = FastAPI()
 
 models.Base.metadata.create_all(engine)
@@ -25,12 +25,12 @@ def create(request: schemas.Blog, db : Session = Depends(get_db)):
     return new_blog
 
 
-@app.get('/blog')
+@app.get('/blog', response_model=List[schemas.ShowBlog])
 def get_all(db : Session = Depends(get_db)):
     blogs = db.query(models.Blog).all()
     return blogs
 
-@app.get('/blog/{id}', status_code= 200)
+@app.get('/blog/{id}', status_code= 200, response_model= schemas.ShowBlog)
 def get_all(id, response: Response, db : Session = Depends(get_db)):
     blog = db.query(models.Blog).filter(models.Blog.id == id).first()
     if not blog:
@@ -51,8 +51,22 @@ def destroy(id, db : Session = Depends(get_db)):
     return {'detail':'done'}
 
 @app.put('/blog/{id}',status_code= 202)
-def update(id,request: schemas.Blog, db : Session = Depends(get_db)):
-    blog = db.query(models.Blog).filter(models.Blog.id == id)\
-        .update({'title':'update'},synchronize_session = False)
+def update_blog(id,request: schemas.Blog, db : Session = Depends(get_db)):
+    blog = db.query(models.Blog).filter(models.Blog.id == id)
+    if not blog:
+        raise HTTPException(status_code= status.HTTP_404_NOT_FOUND, detail = f'Blog with the id = {id} can no')
+    blog.update(request.dict())
     db.commit()
     return {'detail': 'update'}
+
+
+pwd_context = CryptContext(schemes=['bcrypt'], deprecated = 'auto')
+
+@app.post('/user')
+def create_user(request: schemas.User, db : Session = Depends(get_db)):
+    hashedPassword = pwd_context.hash(request.password)
+    new_user = models.User(name = request.name, email = request.email, password = hashedPassword)
+    db.add(new_user)
+    db.commit()
+    db.refresh(new_user)
+    return request
--- Sample/blog/schemas.py
+++ Sample/blog/schemas.py
@@ -1,6 +1,16 @@
 from pydantic import  BaseModel
+from .database import Base
 
 
 class Blog(BaseModel):
     title:str
     body:str
+
+class ShowBlog(Blog):
+    class Config():
+        orm_mode = True
+
+class User(BaseModel):
+    name:str
+    email:str
+    password:str
