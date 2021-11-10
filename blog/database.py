9b0b49b15d78a59d72b2365af01139117a2c358c
Vy Hùng <vyvanhung@Vys-MacBook-Pro.local>
2021-11-11 00:20:47 +0700

    test 3

--- blog/database.py
+++ blog/database.py
@@ -1,8 +1,8 @@
 from sqlalchemy import create_engine
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import  sessionmaker
-import config
-SQLALCHAMY_DATABASE_URL = config.Settings.POSTGRES_DB_URL
+
+SQLALCHAMY_DATABASE_URL = "postgresql://htzeqecigkzlle:4aa44a233f7aa76396fcce7cf873957c323440c3a0cdc4241b09a2d11e5fafeb@ec2-3-227-149-67.compute-1.amazonaws.com:5432/d8iibcolsmi9m7"
 engine = create_engine(SQLALCHAMY_DATABASE_URL,echo =True)
 
 
