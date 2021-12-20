40ef9496b27e9f271e321332e8eb9958552dda7c
Vy Hùng <vyhung@Vys-MacBook-Pro.local>
2021-12-20 09:44:54 +0700

    ktpm test api

--- blog/database.py
+++ blog/database.py
@@ -2,7 +2,7 @@ from sqlalchemy import create_engine
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import  sessionmaker
 
-SQLALCHAMY_DATABASE_URL = "postgresql://htzeqecigkzlle:4aa44a233f7aa76396fcce7cf873957c323440c3a0cdc4241b09a2d11e5fafeb@ec2-3-227-149-67.compute-1.amazonaws.com:5432/d8iibcolsmi9m7"
+SQLALCHAMY_DATABASE_URL = "postgres://stcfrdskwngtzz:480feb19c356fb542e5fc38ee95fabf0b55e06a224649b7ecde8653d820e444e@ec2-52-86-177-34.compute-1.amazonaws.com:5432/d8he5lvhbhonhn"
 engine = create_engine(SQLALCHAMY_DATABASE_URL,echo =True)
 
 
